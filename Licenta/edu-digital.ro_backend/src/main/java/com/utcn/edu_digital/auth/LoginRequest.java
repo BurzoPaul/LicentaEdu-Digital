555ad5e17cc51a3d2482da6e226f0567414b3bd3
BurzoPaul <161871461+BurzoPaul@users.noreply.github.com>
2025-07-06 16:48:56 +0300

    Delete Licenta directory

--- Licenta/edu-digital.ro_backend/src/main/java/com/utcn/edu_digital/auth/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.utcn.edu_digital.auth;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String login;
-    private String password;
-}
--- Licenta/edu-digital.ro_backend/src/main/java/com/utcn/edu_digital/posts/PostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utcn.edu_digital.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PostsRepository extends JpaRepository<Posts, Integer> {
-    List<Posts> findAllByUserId(int userId);
-}
